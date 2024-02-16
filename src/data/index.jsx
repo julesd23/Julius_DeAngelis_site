b75b5252e9626bcb009c0473823e4cfb4ad82894
Julius DeAngelis <juliusdeangelis@gmail.com>
2024-02-16 13:40:10 -0500

    fix project data

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -30,20 +30,20 @@ export const skills = [
 
 export const portfolio = [
   {
-    id: "1",
-    src: taskTrackerPic,
-    title: "Full Stack Task Tracker",
-    description:
-      "An excercise in using NextJs with Clerk for authentication, a MongoDB database, and Mantine as a styling library.",
-    link: "https://github.com/julesd23/mytasks",
-  },
-  {
     id: "2",
     src: "https://media.giphy.com/media/DXMyC8EHbL9AqPnvyD/giphy.gif",
     title: "Bad-Bank",
     description:
       "A simulation banking application within the MERN stack where one can create an account, log in, deposit, and withdraw funds.",
     link: "https://github.com/julesd23/bad-bank",
+  },
+  {
+    id: "1",
+    src: taskTrackerPic,
+    title: "Full Stack Task Tracker",
+    description:
+      "An excercise in using NextJs with Clerk for authentication, a MongoDB database, and Mantine as a styling library.",
+    link: "https://github.com/julesd23/mytasks",
   },
   {
     id: "3",
